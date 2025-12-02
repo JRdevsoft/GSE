68433ee0ac16604841118b8169508ab7087241b7
JRdevsoft <jr.devsoft@gmail.com>
2025-12-02 12:09:24 -0400

    upadateSupabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://imfolqngdrsymfotwiib.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltZm9scW5nZHJzeW1mb3R3aWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MjE3MzYsImV4cCI6MjA1NzM5NzczNn0.RyL2blevo_8Jt1lUbAzs1YuB5OY_3Oux3GDNjZvhCiQ',
+  supabaseUrl: 'https://adryaovnvageedimmbmg.supabase.co', //'https://imfolqngdrsymfotwiib.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcnlhb3ZudmFnZWVkaW1tYm1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0MTA0NDAsImV4cCI6MjA3Mzk4NjQ0MH0.4MzEcSd2Rmsr4SPs6w8adFhb29xElveBU-DLUQIV42g', //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltZm9scW5nZHJzeW1mb3R3aWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MjE3MzYsImV4cCI6MjA1NzM5NzczNn0.RyL2blevo_8Jt1lUbAzs1YuB5OY_3Oux3GDNjZvhCiQ',
   emailjs: {
     serviceId: 'service_1vwb5o5',
     templateIdUser: 'template_52mb6lr',
